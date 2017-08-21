6d279f6cafd18ad22726b3d6035a0482d199e068
Zhirui Li <zr.public@outlook.com>
2017-08-21 10:15:15 +0800

    Remove redundant statement in showToolTip

--- src/main/scala/ui/Utils.scala
+++ src/main/scala/ui/Utils.scala
@@ -5,7 +5,6 @@ import scalafx.scene.control.{Control, Tooltip}
 object Utils {
 
   def showTooltip(control: Control, tooltip: Tooltip, offsetX: Double = 0, offsetY: Double = 0): Unit = {
-    control.tooltip = tooltip
     val p = control.localToScene(0.0, 0.0)
     val win = control.getScene.getWindow
     tooltip.show(
